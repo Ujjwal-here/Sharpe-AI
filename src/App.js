59bd9c7de6d8177c0a631e7ea4312bb379f2326d
Ujjwal-here <ujjwalsbx@gmail.com>
2023-12-02 00:08:41 +0530

    Navigation Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import {Navbar} from "./components/Navbar";
+import {Home} from "./components/Home";
+import {Route, Routes} from "react-router-dom";
+import {Transaction} from "./components/Transaction";
+import {Data} from "./components/Data";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <>
+          <Navbar/>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/transaction" element={<Transaction/>} />
+              <Route path="/data" element={<Data/>} />
+          </Routes>
+      </>
   );
 }
 
