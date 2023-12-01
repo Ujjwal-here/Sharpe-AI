5da2169437c8b0ba5447f9078bfb80799ba94a12
Ujjwal-here <ujjwalsbx@gmail.com>
2023-12-02 00:25:13 +0530

    Add navigation links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import {Link} from "react-router-dom";
+
 export const Navbar = () => {
     return (
         <nav className="bg-[#101012] px-44 py-8">
@@ -6,8 +8,8 @@ export const Navbar = () => {
                     <a href="#" className="text-white text-2xl font-bold">Sharpe AI</a>
                 </div>
                 <div className="flex items-center space-x-4">
-                    <a href="#" className="text-slate-300 mx-10 text-base hover:text-white transition duration-300">Transaction</a>
-                    <a href="#" className="text-slate-300 mx-10 text-base hover:text-white transition duration-300">Data</a>
+                    <Link to="/transaction" href="#" className="text-slate-300 mx-10 text-base hover:text-white transition duration-300">Transaction</Link>
+                    <Link to="/data" href="#" className="text-slate-300 mx-10 text-base hover:text-white transition duration-300">Data</Link>
                 </div>
             </div>
         </nav>
