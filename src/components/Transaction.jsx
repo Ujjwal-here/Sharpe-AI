85b50bf74ca78cf8005e08dedc6c9eeaded93405
Ujjwal-here <ujjwalsbx@gmail.com>
2023-12-02 15:06:28 +0530

    Event Handlers for getting User Inputs

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,15 +1,28 @@
+import {useState} from "react";
+
 export const Transaction = () => {
+
+    const [walletAddress,setWalletAddress]=useState("")
+    const [amount,setAmount]=useState("")
+
+   function walletAddressHandler(e){
+        setWalletAddress(e.target.value)
+   }
+   function amountHandler(e){
+        setAmount(e.target.value)
+   }
+   console.log(walletAddress,amount)
     return (
         <div className="max-w-md mx-auto my-44">
             <form className="space-y-8">
                 <div>
                     <label htmlFor="walletAddress" className="text-lg block text-white mb-4 font-bold">Wallet Address</label>
-                    <input type="text" id="walletAddress" name="walletAddress" placeholder="Enter wallet address"
-                           className="text-sm block border border-white w-full rounded bg-[#101012] py-3 px-3 text-white focus:outline-none focus:ring focus:border-slate-800"/>
+                    <input type="text" id="walletAddress" name="walletAddress" placeholder="Enter wallet address" value={walletAddress} onChange={(e)=>walletAddressHandler(e)}
+                           pattern="^0x[a-fA-F0-9]{40}$" className="text-sm block border border-white w-full rounded bg-[#101012] py-3 px-3 text-white focus:outline-none focus:ring focus:border-slate-800"/>
                 </div>
                 <div>
                     <label htmlFor="amount" className="text-lg block text-white mb-4 font-bold">Amount</label>
-                    <input type="number" id="amount" name="amount" placeholder="Enter amount"
+                    <input type="number" id="amount" name="amount" placeholder="Enter amount" value={amount} onChange={(e)=>amountHandler(e)}
                            className="text-sm block border border-white w-full rounded bg-[#101012] py-3 px-3 text-white focus:outline-none focus:ring focus:border-blue-300"/>
                 </div>
                 <div>
